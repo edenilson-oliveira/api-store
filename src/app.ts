aee4f738a095ffa81aaec085298740ba51330915
Edenilson Oliveira <edenilsonoliveira270@gmail.com>
2024-02-13 22:24:29 -0300

    create server

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+export class App{
+  public server: express.Application;
+  
+  constructor(){
+    this.server = express();
+    this.middleware();
+  }
+  
+  private middleware(){
+    //this.server.use(express.json())
+  }
+}
+
+//export default App
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv';
+dotenv.config()
+
+import { App } from './app.ts';
+
+const app = new App()
+
+app.server.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`)
+})
