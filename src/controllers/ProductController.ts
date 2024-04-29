b99c08b93f019598a889d44ecba6f26d9f363b39
Edenilson Oliveira <edenilsonoliveira270@gmail.com>
2024-04-28 23:06:31 -0300

    Start add get product from search

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -1,5 +1,7 @@
 import { Request,Response } from 'express';
+import { Op } from 'sequelize';
 import Product from '../database/models/product';
+
 
 class ProductController{
   public async getProducts(req: Request,res: Response){
@@ -10,11 +12,37 @@ class ProductController{
       
       res.status(200).json(products)
     }
-    catch(err){
-      console.log(err)
+    catch{
       res.status(500).json({message: 'Internal server error'})
     }
   }
+  
+  public async getProductFromSearch(req: Request,res: Response){
+    try{
+      const { search } = req.params
+      
+      if(!search){
+        return res.status(401).json({message: 'Search not provided'})
+      }
+      
+      const products = await Product.findAll({
+        where: {
+          name: { [Op.like]: `%${search}%`}
+        },
+        limit: 20
+      })
+      
+      if(products.length > 0){
+        return res.status(200).json(products)
+      }
+      
+      res.status(404).json({message: 'Products not found'})
+    }
+    catch{
+      res.status(500).json({message: 'Internal server error'})
+    }
+  }
+  
 }
 
 export default new ProductController
--- src/routes/products.ts
+++ src/routes/products.ts
@@ -4,5 +4,6 @@ import productController from '../controllers/ProductController';
 const router = express.Router()
 
 router.get('/users/products', productController.getProducts)
+router.get('/users/products/:search', productController.getProductFromSearch)
 
 export default router
