bf0d1543f594709397e351fc6deaccedd3a04e7c
Edenilson Oliveira <edenilsonoliveira270@gmail.com>
2024-05-03 23:55:19 -0300

    Start add product seller actions controller and add get products of store

---
+++ src/controllers/ProductSellerActionsController.ts
@@ -0,0 +1,37 @@
+import { Request,Response } from 'express';
+import Product from '../database/models/product';
+import verifyTokenUser from '../authentication/VerifyToken';
+import VerifyUserIsSeller from '../repository/VerifyUserIsSeller';
+
+class ProductSellerActionsController{
+  public async getProductsOfStore(req: Request,res: Response){
+    try{
+      const verifyToken = verifyTokenUser.execute(req,res)
+        const id = verifyToken.userId || 0
+          
+      if(!verifyToken.auth){
+        return
+      }
+        
+      const verifyUserIsSeller = new VerifyUserIsSeller(id)
+        
+      const verify = await verifyUserIsSeller.execute()
+        
+      if(verify){
+        return res.status(401).json({message: verify})
+      }
+      
+      const products = Product.findAll({
+        where: {
+          sellerId: id
+        },
+        limit: 20
+      })
+      
+      res.status(200).json(products)
+    }
+  catch{
+    res.status(500).json({message: 'Internal server error'})
+    }
+  }
+}
