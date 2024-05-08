b7b5e1af884fab590899e9cac9c018a3b5d4812d
Edenilson Oliveira <edenilsonoliveira270@gmail.com>
2024-05-07 23:49:11 -0300

    Add verify is a number

--- src/services/validateInfoAboutProduct.ts
+++ src/services/validateInfoAboutProduct.ts
@@ -1,5 +1,4 @@
 class ValidateInfoAboutProduct{
-  
   private verifyValueExistAndLength(value: string | number,valueName:string,valueLengthMax?: number): never | undefined{
     if(!value){
       throw new Error(`Error ${valueName} is required`)
@@ -9,12 +8,20 @@ class ValidateInfoAboutProduct{
     }
   }
   
-  public validateAllInfo(name: string,price: number,quantity: number,discount: number,description: string): string | undefined{
+  private verifyIsNumber(value: string,valueName: string){
+    const verify = Number(value)
+    if(verify != 0 && !verify)
+    throw new Error(`Error ${valueName} is not a number`)
+  }
+  
+  public validateAllInfo(name: string,price: string,quantity: string,discount: string,description: string): string | undefined{
     try{
       this.verifyValueExistAndLength(name,'name',100)
-      this.verifyValueExistAndLength(price,'price')
-      this.verifyValueExistAndLength(quantity,'quantity')
       this.verifyValueExistAndLength(description,'description',200)
+      
+      this.verifyIsNumber(price,'price')
+      this.verifyIsNumber(quantity,'quantity')
+      this.verifyIsNumber(discount,'discount')
     }
     catch(err: any){
       return err.message
