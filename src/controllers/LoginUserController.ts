5d90f5a736941a44d297cac5ce18f6146ce4ff3c
edenilson-oliveira <edenilsonoliveira270@gmail.com>
2024-08-17 20:36:40 -0300

    Add new routes for change password on login

--- src/controllers/LoginUserController.ts
+++ src/controllers/LoginUserController.ts
@@ -116,6 +116,100 @@ class LoginUserControler{
       res.status(500).json({message: 'Internal server error'})
     }
   }
+
+  public async SendCodeForChangePassword(req: Request,res: Response){
+    try{
+      const {email} = req.body
+
+      if(!email){
+        return res.status(400).json({message: 'Email not was provided'})
+      }
+
+      const emailVerify = new EmailVerify(email || '')
+
+      const emailVerification = await emailVerify.execute()
+
+      if(!emailVerification.emailExists){
+        return res.status(404).json({message: 'Email not found'})
+      }
+
+      const code = new CodeGenerate().execute()
+        
+      const sendMail = new SendMail(email,'Confirm Email', `<p>Confirm your email with code ${code} for to change your password</p>`)
+      await sendMail.execute()
+        
+      client.set(`code-change-password-${email}`, code)
+        
+      client.expire(`code-change-password-${email}`, 180)
+
+      res.status(200).json({message: 'Confirm your email'})
+    }
+    catch{
+      res.status(500).json({message: 'Internal server error'})
+    }
+  }
+
+  public async confirmCodeForChangePassword(req: Request,res: Response){
+    try{
+      const {email} = req.body
+
+      if(!email){
+        return res.status(400).json({message: 'Email not was provided'})
+      }
+
+      const code = await client.get(`code-change-password-${email}`)
+
+      if(!code){
+        return res.status(404).json({message: 'Code not was provided'})
+      }
+
+      if(req.body.code === code){
+        client.del(`code-change-password-${email}`)
+        client.set(`user-email-${email}-code-was-confirmed`,'1')
+        client.expire(`code-change-password-${email}`, 180)
+
+        return res.status(200).end()
+      }
+      
+      res.status(400).json({message: 'Code is invalid'})
+    }
+    catch{
+      res.status(500).json({message: 'Internal server error'})
+    }
+  }
+
+  public async changePassword(req: Request,res: Response){
+    try{
+      const {email,password} = req.body
+
+      if(!email){
+        return res.status(400).json({message: 'Email not was provided'})
+      }
+
+      const codeWasConfirmed = await client.get(`user-email-${email}-code-was-confirmed`)
+
+      if(!Number(codeWasConfirmed)){
+        return res.status(400).json({message: 'Error with code'})
+      }
+
+      if(!(password.length >= 8)){
+        return res.status(400).json({message: 'Password must be at least 8 characters long'})
+      }
+
+      const hashPassword = await bcrypt.hash(password,10)
+
+      await Promise.all([await User.update({password: hashPassword},{
+        where: {
+          email
+        }
+      }),client.del(`user-email-${email}-code-was-confirmed`)])
+
+      res.status(200).end()
+    }
+    catch{
+      res.status(500).json({message: 'Internal server error'})
+    }
+  }
 }
 
 const loginUserControler = new LoginUserControler
